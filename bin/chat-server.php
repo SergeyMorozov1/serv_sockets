48bc47c538cc9d6e37c18590729f48f65f073356
SergeyMorozov1 <morozov.cnc@ya.ru>
2020-08-28 15:20:13 +0600

    new

---
+++ bin/chat-server.php
@@ -0,0 +1,21 @@
+<?php
+
+use MyApp\Chat;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new Chat()
+            )
+        ),
+        8081
+    );
+
+    $server->run();
